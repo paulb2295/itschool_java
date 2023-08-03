4ee726264280b1846873502e6c2879550b81900e
paulb2295 <borleapaul@gmail.com>
2023-08-03 20:58:22 +0300

    session 3 practice

---
+++ src/session3/practice/IncrementDecrementOperators.java
@@ -0,0 +1,12 @@
+package session3.practice;
+
+public class IncrementDecrementOperators {
+
+    public static void main(String[] args) {
+        int count = 10;
+        count ++;
+        System.out.println("After increment: " + count);
+        count --;
+        System.out.println("After decrement " + count);
+    }
+}
